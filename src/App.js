06fc91dc2b468803aece73fbbc6d4fa40f887784
Ryan Tarry <ryantarry@gmail.com>
2022-04-21 19:22:58 +0100

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import "./app.css";
+
+//Import the pages
+
+import Page1 from "./Components/page1"
+import Page2 from "./Components/page2"
+import Page3 from "./Components/page3"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<h1>Birthday Countdown</h1>} />
+          <Route exact path="page1" element={<Page1 />} />
+          <Route exact path="page2" element={<Page2 />} />
+          <Route exact path="page3" element={<Page3 />} />
+        </Routes>
+        <div className="list">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="page1">Countdown to Rudy's Birthday</Link></li>
+            <li><Link to="page2">Countdown to Ryan's Birthday</Link></li>
+            <li><Link to="page3">Countdown to Portia's Birthday</Link></li>
+          </ul>
+        </div>
+      </Router>
     </div>
   );
 }
